80cce0d1f9e9cea583184c55f96b4af07ed9fd92
Rudy <rudy.logre@viacesi.fr>
2018-11-30 11:23:41 +0100

    separation design + dev

--- Components/QuizCateDesign.js
+++ Components/QuizCateDesign.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {StyleSheet, Text, View, TouchableOpacity, Image, ImageBackground} from 'react-native';
 
-export default class ShopCateDesign extends React.Component {
+export default class QuizCateDesign extends React.Component {
     render() {
         return (
             <ImageBackground source={require('../assets/Images/wall.jpg')}
